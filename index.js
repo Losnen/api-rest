e4fcc6bea54236f647c76049d712fcec841a8661
Samuel Ramos <alu0100815146@ull.edu.es>
2017-04-18 22:38:27 +0100

    Añadiendo base de datos

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Product = require('./models/product')
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -9,10 +12,42 @@ const port = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.get('/hola/:name', (req,res) => {
-  res.send({ message: `Hola ${req.params.name}` });
+app.get('/api/product', (req, res) => {
+  res.send(200,{ products: []});
 });
 
-app.listen(port, () => {
-  console.log(`Servidor corriendo en localhost:${port}`)
+app.post('/api/product', (req, res) => {
+  console.log('POST /api/product');
+  console.log(req.body);
+
+  let product = new Product()
+  product.name = req.body.name;
+  product.picture = req.body.picture;
+  product.price = req.body.price;
+  product.category = req.body.category;
+  product.description = req.body.description;
+
+  product.save((err, productStored) => {
+    if(err) res.status(500).send({ message:`Error al salvar en la base de datos: ${err}` });
+  });
+  res.status(200).send({ product: productStored});
+
+});
+
+app.put('/api/product/:productId', (req, res) => {
+
+});
+
+app.delete('/api/product/:productId', (req, res) => {
+
+});
+
+mongoose.connect('mongodb://localhost/shop', (err, res) => {
+  if(err) {
+    return console.log(`Error al conectar a la base de datos: ${err}`);
+  }
+  console.log('Conexión establecida con la base de datos...');
+  app.listen(port, () => {
+    console.log(`Servidor corriendo en localhost:${port}`)
+  });
 });
